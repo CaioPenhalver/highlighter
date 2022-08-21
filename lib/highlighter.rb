fd0fbfd7fabc113c3b5f4d91d5d7e256adafa382
Caio Rodriguez <penhalvercaio@gmail.com>
2022-08-21 02:17:05 +0100

    Feature: add support to assign serializers on the fly

--- lib/highlighter.rb
+++ lib/highlighter.rb
@@ -3,6 +3,8 @@
 require_relative "highlighter/version"
 require_relative "highlighter/serializer"
 require_relative "highlighter/attribute"
+require_relative "highlighter/commands/object_serializer"
+require_relative "highlighter/commands/attribute_serializer"
 
 module Highlighter
   class Error < StandardError; end
--- lib/highlighter/serializer.rb
+++ lib/highlighter/serializer.rb
@@ -7,48 +7,29 @@ module Highlighter
       base.extend(ClassMethods)
     end
 
-    def initialize(object)
+    def initialize(object, options = {})
       @object = object
+      @options = options
     end
 
     def to_h
       return if @object.nil?
 
-      self.class.attributes.each_with_object({}) do |attr, hash|
-        hash[attr.name] = serialize_attribute(attr)
-        hash
-      end
+      Commands::ObjectSerializer.call(object: @object,
+                                      attributes: self.class.attributes,
+                                      options: @options)
+    end
+  end
+
+  # Add methods to expose and set up attributes
+  module ClassMethods
+    def attribute(name, serializer: nil)
+      instance_variable_set(:@attributes, []) unless instance_variable_defined?(:@attributes)
+      instance_variable_get(:@attributes) << Attribute.new(name:, serializer:)
     end
 
-    def serialize_attribute(attr)
-      value = @object.send(attr.name)
-      return serialize_array(value, attr) if value.instance_of? Array
-
-      apply_serializer(value, attr)
-    end
-
-    def serialize_array(array, attr)
-      array.map do |value|
-        apply_serializer(value, attr)
-      end
-    end
-
-    def apply_serializer(value, attr)
-      return value if attr.serializer.nil?
-
-      attr.serializer.new(value).to_h
-    end
-
-    # Add methods to expose and set up attributes
-    module ClassMethods
-      def attribute(name, serializer: nil)
-        instance_variable_set(:@attributes, []) unless instance_variable_defined?(:@attributes)
-        instance_variable_get(:@attributes) << Attribute.new(name:, serializer:)
-      end
-
-      def attributes
-        instance_variable_get(:@attributes)
-      end
+    def attributes
+      instance_variable_get(:@attributes)
     end
   end
 end
